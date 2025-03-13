8061fa1e726fb15f04a35b92db839db674550bd4
José Antonio Juan <joseantoniojuan@facephi.com>
2025-03-13 15:16:22 +0100

    New pod FPHILicenseManager

---
+++ FPHILicenseManager.xcframework/ios-arm64_x86_64-simulator/FPHILicenseManager.framework/Headers/FPHILicenseManager.h
@@ -0,0 +1,20 @@
+//
+//  license_manager.h
+//  license_manager
+//
+//  Created by Rubén Leal on 7/2/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for license_manager.
+FOUNDATION_EXPORT double license_managerVersionNumber;
+
+//! Project version string for license_manager.
+FOUNDATION_EXPORT const unsigned char license_managerVersionString[];
+
+#import <FPHILicenseManager/FPHILicenseManagerObject.h>
+#import <FPHILicenseManager/FPHISDKLicenseManager.h>
+#import <FPHILicenseManager/FPHISDKLicenseComponent.h>
+#import <FPHILicenseManager/FPHILicenseStatus.h>
+
